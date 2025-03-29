e0712b27bd2535567a0ad8c0a9202eb311e279a6
tsly3000 <tsloveyouthreethousand@gmail.com>
2025-03-29 19:32:20 -0400

    P2P Phase 1 Version 1.0.2
    Server saves client data and returns client list upon new client registration.
    In next version:
        = client can create new chat room(socket)
        - client could accept chatting invites
        - get P2P messageing to succesfully work
        - (maybe) database control for local chat message storage

--- client.py
+++ client.py
@@ -1,14 +1,38 @@
 import socket
 import threading
+import json
+import random
 
 # Define basic HOST/PORT parameters
 HOST = '0.0.0.0'
 PORT = 65433
 
 # Get Username, Initiate client Socket and establish Connection
-username = input("Enter your username: ")
+username = input("Enter name: ")
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
+client.send(username.encode("utf-8"))
+
+# Server end has send info of currently available clients
+client_list = json.loads(client.recv(1024).decode("utf-8"))
+if client_list["type"] == "user_list":
+    print("Online users:", client_list["users"]) # change so that user can only see client name later
+
+
+# Do input for client for now, change it to methods like drop list later
+other_client_name = input("Who would you like to talk to?")
+
+if other_client_name:
+    peer_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    peer_host = '0.0.0.0'
+    peer_port = random.randint(62000, 65000)  # change later to take out random
+    peer_server.bind((HOST, PORT))
+    peer_server.listen()
+
+
+
+
+
 
 def receive():
     """
--- server.py
+++ server.py
@@ -1,41 +1,65 @@
 import socket
 import threading
+import json
+import uuid
 
 # Define basic HOST/PORT parameters
 HOST = '0.0.0.0'
 PORT = 65433
 
-# Define Server Socket and Start Listening
+# For now, saving client info in a list of dicts. Switching to SQLite later.
+# Specifically, this should only be for online clients
+CLIENTS = []
+
+
+# Initiate Server Socket and Start Listening
+print("[STARTING] Server is starting...")
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
 server.listen()
 
-# Keep track of clients
-clients = []
 
-# Handle Client - late could be changed to CRUD of clients
-def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
-    while True:
-        try:
-            msg = conn.recv(1024).decode('utf-8')
-            if msg:
-                print(f"[{addr}] {msg}")
-                broadcast(msg, conn)
-        except:
-            clients.remove(conn)
-            conn.close()
-            break
+def get_available_clients(client_id):
+    """
+    Function that takes in id of a client, returns a list of available clients(without self).
 
-# Show broadcast to
-def broadcast(message, sender_conn):
-    for client in clients:
-        if client != sender_conn:
-            client.send(message.encode('utf-8'))
+    Input: 
+        client_name: str, name of client 
+    Output: 
+        available_clients: list, a list of dictionary of available client's info
+    """
+    available_clients = []
+    for client in CLIENTS:
+        if client["name"] != client_name:
+            available_clients.append({"name": client["name"], 
+                                      "addr": addr})
+    return available_clients
 
-print("[STARTING] Server is starting...")
+
+
+# While-Loop that appends client data when new connection established
+# Also send available clients when connection established 
 while True:
     conn, addr = server.accept()
-    clients.append(conn)
-    thread = threading.Thread(target=handle_client, args=(conn, addr))
-    thread.start()
+
+    # Receiving New User Data
+    client_name = conn.recv(1024).decode("utf-8")
+    print(f"[NEW CONNECTION] {addr} connected.")
+    client_id = str(uuid.uuid4())
+    client_info = {"id": client_id,
+                   "name": client_name,
+                   "addr": addr,
+                   "conn": conn}
+    
+
+
+    CLIENTS.append(client_info)
+
+    # Sending Available Client Data to New User
+    available_clients = get_available_clients(client_id)
+    conn.send(json.dumps({"type": "user_list",
+                          "users": available_clients}).encode("utf-8"))
+
+
+    
+# CLIENTS[client_id] = []
